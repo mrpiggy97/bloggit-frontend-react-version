589812911d41170ac1c8f4801e8aaa7c5af7173e
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-02-01 01:22:35 -0500

    moved setUserCredentials const and removeUserCredentials const to Authentication class

---
+++ src/store/utils/Authentication.js
@@ -0,0 +1,50 @@
+class Authentication{
+    constructor(prevState, payload){
+        this.prevState = prevState
+        this.payload = payload
+        this.newState = null
+    }
+
+    removeUserCredentials(){
+        window.localStorage.removeItem('bloggit_token')
+        window.localStorage.removeItem('bloggit_username')
+        window.localStorage.removeItem('bloggit_profile_pic')
+        window.localStorage.removeItem('bloggit_user_communities')
+
+        this.newState = {
+            authenticated : null,
+            username : null,
+            profilePic : null,
+            userCommunities : null,
+        }
+
+        return Object.assign({}, this.prevState, this.newState)
+    }
+
+    setUserCredentials(){
+        window.localStorage.setItem('bloggit_token', this.payload.token)
+        window.localStorage.setItem('bloggit_username', this.payload.username)
+        window.localStorage.setItem('bloggit_profile_pic',  this.payload.profile_pic)
+        window.localStorage.setItem('bloggit_user_communities', this.payload.user_communities)
+
+        this.newState = {
+            authenticated : true,
+            username : this.payload.username,
+            profilePic : this.payload.profile_pic,
+            userCommunities : this.payload.communities
+        }
+
+        return Object.assign({}, this.prevState, this.newState)
+    }
+
+    setAuthenticationState(){
+        if(this.payload.authenticated){
+            return this.setUserCredentials()
+        }
+        else{
+            return this.removeUserCredentials()
+        }
+    }
+}
+
+export default Authentication
