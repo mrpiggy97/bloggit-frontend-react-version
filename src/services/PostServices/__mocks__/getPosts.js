5070d7cf5bbd9ca727dcd8e95381457c044aab46
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-02-07 01:52:38 -0500

    created data for tests, and mock for getPosts

---
+++ src/services/PostServices/__mocks__/getPosts.js
@@ -0,0 +1,27 @@
+import { page1, page2, page3 } from 'tests/utils/dataByPage'
+
+const getPosts = (page) => {
+    let currentPage
+
+    switch (page) {
+        case 1:
+            currentPage = page1
+            break;
+        case 2:
+            currentPage = page2
+            break;
+        case 3:
+            currentPage = page3;
+    
+        default:
+            currentPage = page1
+            break;
+    }
+
+    return Promise.resolve({
+        data : currentPage,
+        status : 200
+    })
+}
+
+export default getPosts
---
+++ src/tests/utils/dataByPage.js
@@ -0,0 +1,100 @@
+export let page1 = {
+    next_page : 2,
+    previous_page : null,
+    results : [
+        {
+            owner : { username : 'testin123123', profile_pic : null },
+            date : "14 april 2022",
+            title : "this is the title of the first post in the first page",
+            text : "this is the text of the fist post in the first page",
+            communities : ['test'],
+            likes : 1,
+            liked : null,
+            reported : null,
+            uuid : '3431213ealTTTTsdsakdldk'
+        },
+
+        {
+            owner : { username : 'anotheruser1', profile_pic : null },
+            date : "14 may 2020",
+            title : "this is the title of the second post in the first page",
+            text : "this is the text of the second post in the first  page",
+            communities : ['test'],
+            likes : 1,
+            liked : null,
+            reported : null,
+            uuid : "123HHSD;k';k4"
+        }
+    ],
+
+    count : 2,
+    authenticated : false,
+}
+
+
+export let page2 = {
+    next_page : 3,
+    previous_page : 1,
+    results : [
+        {
+            owner : { username : 'testin123123', profile_pic : null },
+            date : "14 april 2022",
+            title : "this is the title of the first post in the second page",
+            text : "this is the text of the first post in the second page",
+            communities : ['test'],
+            likes : 1,
+            liked : null,
+            reported : null,
+            uuid : '34ealdsakdldk'
+        },
+
+        {
+            owner : { username : 'anotheruser1', profile_pic : null },
+            date : "14 may 2020",
+            title : "this is the title of the second post in the second page",
+            text : "this is the text of the second post in the second page",
+            communities : ['test'],
+            likes : 1,
+            liked : null,
+            reported : null,
+            uuid : "123;kklj;k4"
+        }
+    ],
+
+    count : 2,
+    authenticated : false,
+}
+
+
+export let page3 = {
+    next_page : null,
+    previous_page : 2,
+    results : [
+        {
+            owner : { username : 'testin123123', profile_pic : null },
+            date : "14 april 2022",
+            title : "this is the title of the first post in the third page",
+            text : "this is the text of the fist post in the third page",
+            communities : ['test'],
+            likes : 1,
+            liked : null,
+            reported : null,
+            uuid : '3431213elllljlha1111122ldsakdldk'
+        },
+
+        {
+            owner : { username : 'anotheruser1', profile_pic : null },
+            date : "14 may 2020",
+            title : "this is the title of the second post in the third page",
+            text : "this is the text of the second post in the third page",
+            communities : ['test'],
+            likes : 1,
+            liked : null,
+            reported : null,
+            uuid : "123;k';5555534k4"
+        }
+    ],
+
+    count : 2,
+    authenticated : false,
+}
