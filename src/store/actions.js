9b131466dfc60d79a0d2b7cb813f1603875c3aaa
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-02-11 18:18:28 -0500

    actions are now wrappes in a single object

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,11 +2,16 @@ import updatePosts from 'store/utils/updatePosts'
 
 
 //these are the actions
-export function resolveUserCredentials(newState){
-    return { type: "RESOLVE_USER_CREDENTIALS", payload : newState }
+
+const actions = {
+    resolveUserCredentials(newState){
+        return { type: "RESOLVE_USER_CREDENTIALS", payload : newState }
+    },
+    //these is an async action
+    updatePosts(url){
+        return { type : "UPDATE_POSTS", payload : updatePosts(url) }
+    }    
 }
 
-//these is an async action
-export function updatePosts(url){
-    return { type : "UPDATE_POSTS", payload : updatePosts(url) }
-}
+
+export default actions
