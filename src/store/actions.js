1a2ac58230b88ee5ab0a473a98eb2d31bbbbbd92
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-02-11 18:08:53 -0500

    mappers have been moved to their own file

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,44 +1,12 @@
-//setters for store
-
 import updatePosts from 'store/utils/updatePosts'
 
-const actions = {
 
-    resolveUserCredentials(newState){
-        return { type: "RESOLVE_USER_CREDENTIALS", payload : newState }
-    },
-
-    updatePosts(url){
-        return { type : "UPDATE_POSTS", payload : updatePosts(url) }
-    }
+//these are the actions
+export function resolveUserCredentials(newState){
+    return { type: "RESOLVE_USER_CREDENTIALS", payload : newState }
 }
 
-export const mapResolveUserCredentialsToProps = (dispatch) => {
-    let action = actions.resolveUserCredentials
-    return {
-        resolveUserCredentials(newState){
-            dispatch(action(newState))
-        }
-    }
+//these is an async action
+export function updatePosts(url){
+    return { type : "UPDATE_POSTS", payload : updatePosts(url) }
 }
-
-export const mapUpdatePostsToProps = (dispatch) => {
-    let action = actions.updatePosts
-    return {
-        updatePosts(url){
-            dispatch(action(url))
-        }
-    }
-}
-
-const mapActionsToProps = (dispatch) => {
-
-    const { resolveUserCredentials } = actions
-    return {
-        resolveUserCredentials(newState){
-            dispatch(resolveUserCredentials(newState))
-        }       
-    }
-}
-
-export default mapActionsToProps
