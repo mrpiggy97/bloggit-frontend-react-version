2da4d22b6f2b3077ebb15e064ce910ccd8894445
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-02-17 22:42:56 -0500

    re wrote the hole test

--- src/tests/HomeView.test.js
+++ src/tests/HomeView.test.js
@@ -1,54 +1,76 @@
 import React from 'react'
-import { act, render } from '@testing-library/react'
-import configStore from 'redux-mock-store'
-import { Provider } from 'react-redux'
 import promise from 'redux-promise-middleware'
+import configStore from 'redux-mock-store'
+import { render, fireEvent } from '@testing-library/react'
+import { unmountComponentAtNode } from 'react-dom'
+import { Provider } from 'react-redux'
 
-import HomeView, { ConnectedHomeView } from 'views/HomeView'
+
+import HomeView from 'views/HomeView'
+import { page1 } from 'tests/utils/dataByPage'
+
+
 
 const middlewares = [promise]
 const mockStore = configStore(middlewares)
-jest.mock('services/PostServices/getPosts')
 
 describe('initial test for HomeView view', () => {
-    let store
-    let wrapper
-    let instance
-    let container
 
-    beforeEach(() => {
-        store = mockStore({
-            authenticated : false,
-            username : null,
-            token : null,
-            userCommunities : null,
-            profilePic : null,
-            posts : [],
-            nextPage : 0,
-            previousPage : 0,
-            fetchingPosts : false,
-            fetchingStatus : {
-                success : null,
-                code : null
-            }
-        })
-
-        store.dispatch = jest.fn()
-
-        container = document.createElement("div")
-        document.body.appendChild(container)
-
-        act(() => {
-
-            let { getByText } = render(
-                <Provider store={store}>
-                    <HomeView/>
-                </Provider>, container
-            )
-        })
+    let store = mockStore({
+        authenticated : false,
+        username : null,
+        token : null,
+        userCommunities : null,
+        profilePic : null,
+        posts : [],
+        nextPage : 0,
+        previousPage : 0,
+        fetchingPosts : false,
+        fetchingStatus : {
+            success : null,
+            code : null
+        }
     })
 
-    it('checks that view has been mounted correctly',  () => {
+    store.dispatch = jest.fn()
+
+    let store2 = mockStore({
+        authenticated : false,
+        username : null,
+        token : null,
+        userCommunities : null,
+        profilePic : null,
+        posts : page1.results,
+        nextPage : page1.next_page,
+        previousPage : page1.previous_page,
+        fetchingPosts : false,
+        fetchingStatus : {
+            success : null,
+            code : null
+        }
+    })
+
+    store2.dispatch = jest.fn()
+
+    it('checks that view has been mounted correctly', () => {
+
+        let wrapper = render(
+            <Provider store={store}>
+                <HomeView/>
+            </Provider>
+        )
+
+        expect(wrapper.getByText('next')).toBeInTheDocument()
         expect(store.dispatch).toHaveBeenCalledTimes(1)
+    })
+
+    it('sit', () => {
+
+        let wrapper = render(
+            <Provider store={store2}>
+                <HomeView/>
+            </Provider>
+        )
+        expect(wrapper.getByText('next page')).toBeInTheDocument()
     })
 })
