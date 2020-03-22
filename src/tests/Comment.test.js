4a1d5e8c0855e95af7c01ab53d98dc21ac8135b0
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-03-22 17:45:22 -0400

    updated test

--- src/tests/Comment.test.js
+++ src/tests/Comment.test.js
@@ -7,56 +7,66 @@ import { Provider } from 'react-redux'
 import Comment, { ConnectedComment } from '../components/comment'
 
 const mockedStore = configStore([])
-let unauthenticatedStore = mockedStore({
-    authenticated : false,
-    posts : [],
-    token : null,
-    username : null,
-    profilePic : null,
-    user_communities : null,
-    nextPage : 0,
-    previousPage : 0,
-    fetchingPosts : false,
-    fetchingStatus : {
-        code : null,
-        success : null
-    }
+
+let unauthenticatedStore
+let authenticatedStore
+
+let info = {
+    owner : { username : 'testingsuser', profile_pic : null },
+    date : "12 april 2020",
+    text : "this is the comment text",
+    likes : 1,
+    liked : null,
+    reported : null,
+    parent_comment : null,
+    has_parent : false,
+    is_original : true
+}
+
+let info2 = { ...info, liked : false, reported : false }
+
+beforeEach(() => {
+    unauthenticatedStore = mockedStore({
+        authenticated : false,
+        posts : [],
+        token : null,
+        username : null,
+        profilePic : null,
+        user_communities : null,
+        nextPage : 0,
+        previousPage : 0,
+        fetchingPosts : false,
+        fetchingStatus : {
+            code : null,
+            success : null
+        }
+    })
+    unauthenticatedStore.dispatch = jest.fn()
+
+    authenticatedStore = mockedStore({
+        authenticated : true,
+        token : 'qe12;lk3kl324',
+        username : 'madara',
+        profilePic : null,
+        user_communities : [],
+        nextPage : 0,
+        previousPage : 0,
+        fetchingPosts : false,
+        fetchingStatus : {
+            code : null,
+            success : null
+        }
+    })
+
+    authenticatedStore.dispatch = jest.fn()
 })
 
-unauthenticatedStore.dispatch = jest.fn()
-
-
-let authenticatedStore = mockedStore({
-    authenticated : true,
-    token : 'qe12;lk3kl324',
-    username : 'madara',
-    profilePic : null,
-    user_communities : [],
-    nextPage : 0,
-    previousPage : 0,
-    fetchingPosts : false,
-    fetchingStatus : {
-        code : null,
-        success : null
-    }
+afterEach(() => {
+    authenticatedStore = null
+    unauthenticatedStore = null
 })
 
-authenticatedStore.dispatch = jest.fn()
-
-describe('test render with authenticated user', () => {
-    let info = {
-        owner : { username : 'testingsuser', profile_pic : null },
-        date : "12 april 2020",
-        text : "this is the comment text",
-        likes : 1,
-        liked : null,
-        reported : null,
-        parent_comment : null,
-        has_parent : false,
-        is_original : true
-    }
-
-    let info2 = { ...info, liked : false, reported : false }
+describe('test initial render and like and report methods', () => {
 
     it('checks that component renders normally', () => {
         let wrapper = render(
@@ -80,5 +90,46 @@ describe('test render with authenticated user', () => {
         expect(wrapper2.getByText("like")).toBeInTheDocument()
         fireEvent.click(wrapper2.getByText("like"))
         expect(wrapper2.getByText("2")).toBeInTheDocument()
+        fireEvent.click(wrapper2.getByText("like"))
+        expect(wrapper2.getByText("2")).toBeInTheDocument()
+    })
+})
+
+describe('test comment object', () => {
+    it("checks that object renders as expected", () => {
+        let wrapper
+        let instance
+        act(() => {
+            wrapper = create(
+                <Provider store={unauthenticatedStore}>
+                    <Comment info={info}/>
+                </Provider>
+            )
+        })
+
+        instance = wrapper.root
+
+        let props = { className : "comment-likes"}
+        expect(instance.findByProps(props).children[0]).toBe("1")
+    })
+
+    it('checks fire events work as expected', () => {
+        let wrapper
+        let instance
+
+        act(() => {
+            wrapper = create(
+                <Provider store={authenticatedStore}>
+                    <Comment info={info2}/>
+                </Provider>
+            )
+        })
+
+        instance = wrapper.root
+        let props = { className : 'like-inactive' }
+        act(() => {
+            instance.findByProps({className : 'like-inactive'}).props.onClick()
+        })
+        expect(instance.findByProps({ className : 'comment-likes'}).children[0]).toBe("2")
     })
 })
