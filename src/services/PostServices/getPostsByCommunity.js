2167b633edfae3132b1e7482ab8a64684cc427aa
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-03-25 18:11:38 -0400

    added service to get posts by community

---
+++ src/services/PostServices/getPostsByCommunity.js
@@ -0,0 +1,20 @@
+import { withoutTokenInstance, authenticateTokenInstance } from '../axiosInstances'
+
+function getPostsByCommunity(page, community){
+    let url = `posts/posts-by-community/${community}/${page}/`
+    let method = 'get'
+    if(window.localStorage.getItem('bloggit_token')){
+        return authenticateTokenInstance({
+            method : method,
+            url : url
+        })
+    }
+    else{
+        return withoutTokenInstance({
+            method : method,
+            url : url
+        })
+    }
+}
+
+export default getPostsByCommunity
