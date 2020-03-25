f4354b2a831ed877558fc12cc5dd23b3829b9dfa
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-03-25 18:17:57 -0400

    updated imports, state, and propTypes, created new methods, and added life cycle method

--- src/views/postsByCommunity.js
+++ src/views/postsByCommunity.js
@@ -1,29 +1,118 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-import PostInfo from '../components/PostInfo'
-import { mapAuthenticatedToProps } from '../store/getters'
-import { mapUpdateCommunityPostsToProps } from '../store/mapActions'
+import { mapUpdateCommunityPostsToProps } from 'store/mapActions'
+import PostInfo from 'components/PostInfo'
 
-class ConnectedPostsByCommunity extends React.Component{
+let mapStoreToProps = (state) => {
+    return {
+        authenticated : state.authenticated,
+        posts : state.posts,
+        nextPage : state.nextPage,
+        previousPage : state.previousPage,
+        fetchingStatus : state.fetchingStatus,
+        fetchingPosts : state.fetchingPosts
+    }
+}
+
+export class ConnectedPostsByCommunity extends React.Component{
     constructor(props){
         super(props)
 
         this.state = {
-            posts : props.state.posts,
-            nextPage : props.state.nextPage,
-            previousPage : props.state.previousPage,
-            fetchingPosts : props.state.fetchingPosts,
-            fetchingStatus : props.state.fetchingStatus,
-            isAuthenticated : props.state.authenticated,
-            community : ""
+            community : this.props.match.params.community,
+            isAuthenticated : this.props.authenticated,
+            posts : this.props.posts,
+            nextPage : this.props.nextPage,
+            previousPage : this.props.previousPage,
+            fetchingPosts : this.props.fetchingPosts,
+            fetchingStatus : this.props.fetchingStatus
+        }
+
+        this.updatePosts = this.updatePosts.bind(this)
+        this.getNextPagePosts = this.getNextPagePosts.bind(this)
+        this.getPreviousPagePosts = this.getPreviousPagePosts.bind(this)
+    }
+
+    updatePosts(page){
+        this.props.updateCommunityPosts(page, this.community)
+    }
+
+    getNextPagePosts(){
+        if(!this.state.nextPage){
+            return null
+        }
+        this.updatePosts(this.state.nextPage)
+    }
+
+    getPreviousPagePosts(){
+        if(!this.state.previousPage){
+            return null
+        }
+        this.updatePosts(this.state.previousPage)
+    }
+
+    componentDidMount(){
+        this.updatePosts(1)//initial page
+    }
+
+    componentDidUpdate(prevProps){
+        if(prevProps !== this.props){
+            this.setState({
+                isAuthenticated : this.props.authenticated,
+                posts : this.props.posts,
+                nextPage : this.props.nextPage,
+                previousPage : this.props.previousPage,
+                fetchingPosts : this.props.fetchingPosts,
+                fetchingStatus : this.props.fetchingStatus
+            })
         }
     }
 
     render(){
         return(
-            <div></div>
+            <div id="posts-by-community">
+                <span className="community" >{this.state.community}</span>
+                <div className="posts">
+                    {this.state.posts.map(post => {
+                        return <PostInfo
+                                info={post}
+                                isPreview={true}
+                                isAuthenticated={this.state.isAuthenticated}
+                                />
+                    })}
+                </div>
+                <div className="arrows">
+                    {this.state.previousPage
+                        ?   <span className="previous-page active" onClick={this.getPreviousPagePosts}>
+                                previous
+                            </span>
+                        :   <span className="previous-page inactive">previous</span>
+                    }
+                    {this.state.nextPage
+                        ?   <span className="next-page active" onClick={this.getNextPagePosts}>
+                                next
+                            </span>
+                        :   <span className="next-page inactive">next</span>
+                    }
+                </div>
+            </div>
         )
     }
 }
+
+ConnectedPostsByCommunity.propTypes = {
+    authenticated : PropTypes.bool.isRequired,
+    posts : PropTypes.object.isRequired,
+    nextPage : PropTypes.number.isRequired,
+    previousPage : PropTypes.number.isRequired,
+    fetchingPosts : PropTypes.bool.isRequired,
+    fetchingStatus : PropTypes.object.isRequired,
+    updateCommunityPosts : PropTypes.func.isRequired
+}
+
+const postsByCommunity = connect(mapStoreToProps, mapUpdateCommunityPostsToProps)
+                                (ConnectedPostsByCommunity)
+
+export default postsByCommunity
