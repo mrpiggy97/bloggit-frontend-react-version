9a6f98bff817631ab1549d489bac52d292e90fec
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-03-30 10:34:07 -0400

    updated imports, replaced stores

--- src/tests/HomeView.test.js
+++ src/tests/HomeView.test.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import promise from 'redux-promise-middleware'
-import configStore from 'redux-mock-store'
 import { render, fireEvent } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -8,53 +6,25 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import HomeView from 'views/HomeView'
 import { page1 } from 'tests/utils/dataByPage'
-
-
-const middlewares = [promise]
-const mockStore = configStore(middlewares)
+import { unauthenticatedStore } from 'tests/utils/storeMocks'
+import createStore from 'tests/utils/storeMocks'
 
 describe('initial test for HomeView view', () => {
 
-    let store = mockStore({
-        authenticated : false,
-        username : null,
-        token : null,
-        userCommunities : null,
-        profilePic : null,
-        posts : [],
-        nextPage : 0,
-        previousPage : 0,
-        fetchingPosts : false,
-        fetchingStatus : {
-            success : null,
-            code : null
-        }
-    })
-
-    store.dispatch = jest.fn()
-
-    let store2 = mockStore({
-        authenticated : false,
-        username : null,
-        token : null,
-        userCommunities : null,
-        profilePic : null,
+    let mockedStoreWithPosts = createStore({
+        ...unauthenticatedStore,
         posts : page1.results,
         nextPage : page1.next_page,
         previousPage : page1.previous_page,
-        fetchingPosts : false,
-        fetchingStatus : {
-            success : null,
-            code : null
-        }
     })
+    mockedStoreWithPosts.dispatch = jest.fn()
 
-    store2.dispatch = jest.fn()
+    let unauthenticatedMockedStore = createStore({ ...unauthenticatedStore })
+    unauthenticatedMockedStore.dispatch = jest.fn()
 
     it('checks that view has been mounted correctly', () => {
-        let his = {}
         let wrapper = render(
-            <Provider store={store}>
+            <Provider store={unauthenticatedMockedStore}>
                 <Router>
                     <Route render={(props) => <HomeView {...props}/>} />                    
                 </Router>
@@ -62,21 +32,22 @@ describe('initial test for HomeView view', () => {
         )
 
         expect(wrapper.getByText('next')).toBeInTheDocument()
-        expect(store.dispatch).toHaveBeenCalledTimes(1)
+        expect(unauthenticatedMockedStore.dispatch).toHaveBeenCalledTimes(1)
     })
 
     it('checks expected render given render with posts', () => {
 
         let wrapper = render(
-            <Provider store={store2}>
+            <Provider store={mockedStoreWithPosts}>
                 <Router>
                     <Route render={(props) => <HomeView {...props} />} />
                 </Router>
             </Provider>
         )
+
         expect(wrapper.getByText('next page')).toBeInTheDocument()
-        expect(store2.dispatch).toHaveBeenCalledTimes(0)
+        expect(mockedStoreWithPosts.dispatch).toHaveBeenCalledTimes(0)
         fireEvent.click(wrapper.getByText("next page"))
-        expect(store2.dispatch).toHaveBeenCalledTimes(1)
+        expect(mockedStoreWithPosts.dispatch).toHaveBeenCalledTimes(1)
     })
 })
