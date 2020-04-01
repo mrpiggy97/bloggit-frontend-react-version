87b25674306e3fbf2d23962d83af76d69bb6532b
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-04-01 00:17:36 -0400

    added another mock for getPosts

---
+++ src/services/PostServices/__mocks__/getPosts2.js
@@ -0,0 +1,25 @@
+import { fakePage1, fakePage2, fakePage3 } from 'tests/utils/dataByPage'
+
+function getPosts2(page){
+    let currentPage
+    switch(page){
+        case 1:
+            currentPage = fakePage1
+        break
+        case 2:
+            currentPage = fakePage2
+        break
+        case 3:
+            currentPage = fakePage3
+        break
+        default:
+            currentPage = fakePage1
+    }
+
+    return Promise.resolve({
+        data : currentPage,
+        code : 200
+    })
+}
+
+export default getPosts2
