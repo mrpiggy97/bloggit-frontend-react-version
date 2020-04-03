26dc8e91b49a74be443210301120a773ac126f18
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-04-03 00:55:13 -0400

    changed communities

--- src/tests/utils/dataByPage.js
+++ src/tests/utils/dataByPage.js
@@ -7,7 +7,7 @@ export let page1 = {
             date : "14 april 2022",
             title : "this is the title of the first post in the first page",
             text : "this is the text of the fist post in the first page",
-            communities : ['test'],
+            communities : ['test1'],
             likes : 1,
             liked : null,
             reported : null,
@@ -19,7 +19,7 @@ export let page1 = {
             date : "14 may 2020",
             title : "this is the title of the second post in the first page",
             text : "this is the text of the second post in the first  page",
-            communities : ['test'],
+            communities : ['test2'],
             likes : 1,
             liked : null,
             reported : null,
@@ -41,7 +41,7 @@ export let page2 = {
             date : "14 april 2022",
             title : "this is the title of the first post in the second page",
             text : "this is the text of the first post in the second page",
-            communities : ['test'],
+            communities : ['test1'],
             likes : 1,
             liked : null,
             reported : null,
@@ -53,7 +53,7 @@ export let page2 = {
             date : "14 may 2020",
             title : "this is the title of the second post in the second page",
             text : "this is the text of the second post in the second page",
-            communities : ['test'],
+            communities : ['test2'],
             likes : 1,
             liked : null,
             reported : null,
@@ -75,7 +75,7 @@ export let page3 = {
             date : "14 april 2022",
             title : "this is the title of the first post in the third page",
             text : "this is the text of the fist post in the third page",
-            communities : ['test'],
+            communities : ['test1'],
             likes : 1,
             liked : null,
             reported : null,
@@ -87,7 +87,7 @@ export let page3 = {
             date : "14 may 2020",
             title : "this is the title of the second post in the third page",
             text : "this is the text of the second post in the third page",
-            communities : ['test'],
+            communities : ['test2'],
             likes : 1,
             liked : null,
             reported : null,
@@ -104,7 +104,7 @@ let fakePost = {
     date : "12 april 2020",
     title : "this is the title",
     text : "this is the text",
-    communities : ["test"],
+    communities : ["test1"],
     likes : 1,
     liked : null,
     reported : null,
@@ -113,37 +113,31 @@ let fakePost = {
 
 let fakePage1, fakePage2, fakePage3 = null
 
-function fillPage(start, limit, id) {
-    //fill results with 20 versions of the same post
-    if(start < 0 || limit > 100 || id === null){
+function fillPage(numberOfFakedPosts, nextPage, previousPage, id, fakedPost) {
+    //fill results with 20 versions of the same post, this is exactly the
+    //data the api would give us
+    if(numberOfFakedPosts === null || id === null || fakedPost === null){
         return null
     }
 
     let results = []
-    for(let i=start; i < limit; i++){
-        let post = { ...fakePost, uuid : i + id }
+    for(let i=0; i < numberOfFakedPosts; i++){
+        let post = { ...fakedPost, uuid : i + id }
         results.push(post)
     }
 
     let page = {
         results : results,
         count : results.length,
+        next_page : nextPage,
+        previous_page : previousPage
     }
 
     return page
 }
 
-fakePage1 = fillPage(0, 10, 2)
-fakePage2 = fillPage(0, 10, 44)
-fakePage3 = fillPage(0, 10, 5)
+fakePage1 = fillPage(10, 2, null, 3, fakePost)
+fakePage2 = fillPage(10, 3, 1, 44, fakePost)
+fakePage3 = fillPage(10, null, 2, 67, fakePost)
 
-fakePage1.next_page = 2
-fakePage1.previous_page = null
-
-fakePage2.next_page = 3
-fakePage2.previous_page = 1
-
-fakePage3.next_page = null
-fakePage3.previous_page = 2
-
-export { fakePage1, fakePage2, fakePage3 }
+export { fakePage1, fakePage2, fakePage3, fakePost, fillPage }
