a1116834cdff9785f7ae84f375f5db8f96d85b01
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-04-13 20:31:21 -0400

    moved everything related to fake api calls to test/utils/fillPageByCommunity

--- src/services/PostServices/__mocks__/fakePostsByCommunity.js
+++ src/services/PostServices/__mocks__/fakePostsByCommunity.js
@@ -1,39 +1,4 @@
-import { fakePost, fillPage } from 'tests/utils/dataByPage'
-
-let fakePost1 = { ...fakePost, communities : ["test1"] }
-let fakePost2 = { ...fakePost, communities : ["test2"] }
-let fakePost3 = { ...fakePost, communities : ["test3"] }
-
-
-function fillPageByCommunity(community){
-    let page1, page2, page3
-    switch(community){
-        case "test1":
-            page1 = fillPage(10, 2, null, 23, fakePost1)
-            page2 = fillPage(10, 3, 1, 7, fakePost1)
-            page3 = fillPage(10, null, 2, 51, fakePost1)
-        break
-
-        case "test2":
-            page1 = fillPage(10, 2, null, 43, fakePost2)
-            page2 = fillPage(10, 3,  1, 1, fakePost2)
-            page3 = fillPage(10, null, 2, 19, fakePost2)
-        break
-
-        case "test3":
-            page1 = fillPage(10, 2, null, 67, fakePost3)
-            page2 = fillPage(10, 3, 1, 6, fakePost3)
-            page3 = fillPage(10, null, 2, 87, fakePost3)
-        break
-
-        default:
-            page1 = null
-            page2 = null
-            page3 = null
-    }
-
-    return { page1, page2, page3 }
-}
+import fillPageByCommunity from 'tests/utils/fillPageByCommunity'
 
 function fakePostsByCommunity(page, community){
     let { page1, page2, page3 } = fillPageByCommunity(community)
