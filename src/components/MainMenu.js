aed61b4bc4f1d06dbd6c2aa73c3d61c672c0014a
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-05-04 16:34:22 -0400

    added PropTypes, added search method

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import { MapState } from 'store/getters'
 import MapActions from 'store/MapActions'
@@ -21,8 +22,10 @@ function ConnectedMainMenu(props){
         setQuery(e.target.value)
     }
 
-    const searchPosts = () => {
-        console.log(query)
+    const searchPosts = (e) => {
+        e.preventDefault()
+        props.updateResults(1, query)
+        history.push(`/posts/search/${query}`)
     }
 
     const logout = () => {
@@ -54,12 +57,11 @@ function ConnectedMainMenu(props){
         setIsAuthenticated(props.authenticated)
         setUsername(props.username)
     }, [props.authenticated, props.username])
-    
     return(
         <div id="main-menu">
             <div id="home">
                 <span className="go-home" onClick={goToHome}>home</span>
-                <form id="search-posts">
+                <form id="search-posts" onSubmit={searchPosts}>
                     <input type="text" onChange={updateQuery}/>
                     <button type="submit" id="search-posts">search</button>
                 </form>
@@ -77,6 +79,14 @@ function ConnectedMainMenu(props){
             }
         </div>
     )
+}
+
+ConnectedMainMenu.propTypes = {
+    resolveUserCredentials : PropTypes.func.isRequired,
+    updateResults : PropTypes.func.isRequired,
+    updatePosts : PropTypes.func.isRequired,
+    authenticated : PropTypes.bool.isRequired,
+    username : PropTypes.string||null.isRequired,
 }
 
 const MainMenu = connect(stateMapper.MainMenuStateToProps, actionMapper.MainMenuActionsToProps)
