c11920bb0f720e0a40ebaa02d78f1b775548325f
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-05-03 20:56:18 -0400

    deleted unnessesary fake pages

--- src/tests/utils/fillPage.js
+++ src/tests/utils/fillPage.js
@@ -1,109 +1,4 @@
-//while it seems redudant to have page1, page2 and page3 when fakePage1
-//fakePage2 and fakePage3 are available, these last pages only contain the same
-//posts all with the same title, text, likes owner ect, while page1, page2,
-//and page3 all have different title text and owner, this trying to tell which
-//which data we are supposed to see
 
-let page1 = {
-    next_page : 2,
-    previous_page : null,
-    results : [
-        {
-            owner : { username : 'testin123123', profile_pic : null },
-            date : "14 april 2022",
-            title : "this is the title of the first post in the first page",
-            text : "this is the text of the fist post in the first page",
-            communities : ['test1'],
-            likes : 1,
-            liked : null,
-            reported : null,
-            uuid : '3431213ealTTTTsdsakdldk'
-        },
-
-        {
-            owner : { username : 'anotheruser1', profile_pic : null },
-            date : "14 may 2020",
-            title : "this is the title of the second post in the first page",
-            text : "this is the text of the second post in the first  page",
-            communities : ['test2'],
-            likes : 1,
-            liked : null,
-            reported : null,
-            uuid : "123HHSD;k';k4"
-        }
-    ],
-
-    count : 2,
-    authenticated : false,
-}
-
-
-let page2 = {
-    next_page : 3,
-    previous_page : 1,
-    results : [
-        {
-            owner : { username : 'testin123123', profile_pic : null },
-            date : "14 april 2022",
-            title : "this is the title of the first post in the second page",
-            text : "this is the text of the first post in the second page",
-            communities : ['test1'],
-            likes : 1,
-            liked : null,
-            reported : null,
-            uuid : '34ealdsakdldk'
-        },
-
-        {
-            owner : { username : 'anotheruser1', profile_pic : null },
-            date : "14 may 2020",
-            title : "this is the title of the second post in the second page",
-            text : "this is the text of the second post in the second page",
-            communities : ['test2'],
-            likes : 1,
-            liked : null,
-            reported : null,
-            uuid : "123;kklj;k4"
-        }
-    ],
-
-    count : 2,
-    authenticated : false,
-}
-
-
-let page3 = {
-    next_page : null,
-    previous_page : 2,
-    results : [
-        {
-            owner : { username : 'testin123123', profile_pic : null },
-            date : "14 april 2022",
-            title : "this is the title of the first post in the third page",
-            text : "this is the text of the fist post in the third page",
-            communities : ['test1'],
-            likes : 1,
-            liked : null,
-            reported : null,
-            uuid : '3431213elllljlha1111122ldsakdldk'
-        },
-
-        {
-            owner : { username : 'anotheruser1', profile_pic : null },
-            date : "14 may 2020",
-            title : "this is the title of the second post in the third page",
-            text : "this is the text of the second post in the third page",
-            communities : ['test2'],
-            likes : 1,
-            liked : null,
-            reported : null,
-            uuid : "123;k';5555534k4"
-        }
-    ],
-
-    count : 2,
-    authenticated : false,
-}
 
 let fakePost = {
     owner : { username : 'fakeuser14234', profilePic : null },
@@ -116,8 +11,6 @@ let fakePost = {
     reported : null,
     uuid : null
 }
-
-let fakePage1, fakePage2, fakePage3 = null
 
 function fillPage(nextPage, previousPage, id, fakedPost) {
     //fill results with 5 versions of the same post, this is exactly the
@@ -140,10 +33,10 @@ function fillPage(nextPage, previousPage, id, fakedPost) {
     return page
 }
 
-fakePage1 = fillPage(2, null, 3, fakePost)
-fakePage2 = fillPage(3, 1, 44, fakePost)
-fakePage3 = fillPage(null, 2, 67, fakePost)
+let fakePage1 = fillPage(2, null, 3, fakePost)
+let fakePage2 = fillPage(3, 1, 44, fakePost)
+let fakePage3 = fillPage(null, 2, 67, fakePost)
 
-export { fakePage1, fakePage2, fakePage3, fakePost, page1, page2, page3 }
+export { fakePage1, fakePage2, fakePage3, fakePost }
 
 export default fillPage
