5528fa16e73cdd1640a9af1be0b61c9c2c22a479
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-05-06 00:00:42 -0400

    useEffect is no longer needed, removed unnessesary use of state

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
@@ -12,8 +12,6 @@ let stateMapper = new MapState()
 let actionMapper = new MapActions()
 
 function ConnectedMainMenu(props){
-    const[IsAuthenticated, setIsAuthenticated] = useState(props.authenticated)
-    const [username, setUsername] = useState(props.username)
     const [query, setQuery] = useState("")
     const history = useHistory()
 
@@ -50,13 +48,6 @@ function ConnectedMainMenu(props){
         props.updatePosts(1)
         history.push("/")
     }
-
-    //when store state.authenticated or state.posts get updated run effect
-    //and change them
-    useEffect(() => {
-        setIsAuthenticated(props.authenticated)
-        setUsername(props.username)
-    }, [props.authenticated, props.username])
     return(
         <div id="main-menu">
             <div id="home">
@@ -67,10 +58,10 @@ function ConnectedMainMenu(props){
                 </form>
                 <span>communities</span>
             </div>
-            {IsAuthenticated ?
+            {props.authenticated ?
                 <div id="user">
-                    <span>{username}</span>
-                    <span onClick={logout} >logout</span>
+                    <span>{props.username}</span>
+                    <span onClick={logout}>logout</span>
                 </div> :
                 <div id="user">
                     <span onClick={login}>login</span>
@@ -86,7 +77,7 @@ ConnectedMainMenu.propTypes = {
     updateResults : PropTypes.func.isRequired,
     updatePosts : PropTypes.func.isRequired,
     authenticated : PropTypes.bool.isRequired,
-    username : PropTypes.string||null.isRequired,
+    username : PropTypes.string
 }
 
 const MainMenu = connect(stateMapper.MainMenuStateToProps, actionMapper.MainMenuActionsToProps)
