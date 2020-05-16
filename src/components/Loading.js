13139058955798b3136ad5ab85eac6904f13efc0
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-05-15 22:29:20 -0400

    created component to indicate page is loading

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'components/css/Loading.css'
+
+function Loading(){
+    return(
+        <div id="loading">
+            <span>loading ...</span>
+        </div>
+    )
+}
+
+export default Loading
