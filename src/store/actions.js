7ef9cb2770374828a2fa69c03809fa430a88d900
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-05-30 00:24:03 -0400

    added new actions and made new imports for them

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,7 @@
 import updatePosts from './utils/updatePosts'
 import updateCommunityPosts from './utils/updateCommunityPosts'
 import updateResults from './utils/updateResults'
+import { Login, Register, Logout } from './utils/Authentication'
 
 
 //all actions should be wrapped inside actions const
@@ -24,6 +25,18 @@ const actions = {
 
     updateResults(page, query){
         return { type : "UPDATE_POSTS", payload : updateResults(page, query)}
+    },
+
+    login(username, password){
+        return { type : "LOGIN", payload : Login(username, password) }
+    },
+
+    register(username, password1, password2){
+        return { type : "LOGIN",  payload : Register(username, password1, password2) }
+    },
+
+    logout(){
+        return { type : "LOGOUT", payload : Logout() }
     }
 }
 
