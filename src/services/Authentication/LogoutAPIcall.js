9c71e6ac910445257b916c56907692506a861a79
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-06-17 14:02:31 -0400

    added slash at the end of url

--- src/services/Authentication/LogoutAPIcall.js
+++ src/services/Authentication/LogoutAPIcall.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let BASE_API_URL = process.env.REACT_APP_BLOGGIT_API_URL
 
 export default function LogoutAPIcall(){
-    let url = `${BASE_API_URL}/rest-auth/logout`
+    let url = `${BASE_API_URL}/rest-auth/logout/`
     return axios({
         method : "POST",
         headers : {
